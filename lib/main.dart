90ed6744b527360b42e3afa0e5cb6307d0628823
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-07-02 23:33:17 +0300

    change files

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:chitchat/screens/login_screen.dart';
+
 import 'package:flutter/material.dart';
+
 
 import 'screens/register_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
   runApp(const ChitChat());
 }
 
@@ -14,15 +18,14 @@ class ChitChat extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        LoginScreen.id:(context)=>const LoginScreen(),
-        RegisterScreen.id:(context)=>const RegisterScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegisterScreen.id: (context) => const RegisterScreen(),
       },
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple,),
         useMaterial3: true,
       ),
-      initialRoute: "LoginScreen",
+      initialRoute: LoginScreen.id,
     );
   }
 }
-
