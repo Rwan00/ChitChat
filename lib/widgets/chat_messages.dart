253721e3b5818c08770d43a646b7bc86ed7e572d
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-07-14 03:46:14 +0300

    get data in stream builder

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,5 +1,9 @@
+import 'package:chitchat/models/messages_model.dart';
+import 'package:chitchat/theme/fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import '../constants/consts.dart';
 import 'chat_bubble.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -7,13 +11,49 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, index) {
-      return MessageBubble.first(
-        username: "Rowan",
-        message: "Hello There",
-        isMe: true,
-        time: "",
-      );
-    });
+    CollectionReference messagesCollection =
+        FirebaseFirestore.instance.collection(kMessagesCollection);
+    return StreamBuilder(
+      stream: messagesCollection.snapshots(),
+      builder: (context, snapshot) {
+         if (snapshot.hasError) {
+          return Center(child: Text('Something went wrong',style: titleStyle,),);
+        }
+
+        else if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+          child:   Image.asset(
+                  kLoading,
+                  width: 90,
+                  height: 90,
+                ),
+          );
+        }
+        else if (snapshot.hasData) {
+          List<MessagesModel> messagesList = [];
+          for (int i = 0; i < snapshot.data!.docs.length; i++) {
+            messagesList.add(MessagesModel.fromJson(snapshot.data!.docs[i]));
+          }
+          return ListView.builder(
+            itemCount: messagesList.length,
+            itemBuilder: (context, index) {
+              return MessageBubble.first(
+                username: "Rowan",
+                message: messagesList[index].message,
+                isMe: true,
+                time: "",
+              );
+            },
+          );
+        } else {
+          return Center(
+            child: Text(
+              "No Messages Found",
+              style: titleStyle,
+            ),
+          );
+        }
+      },
+    );
   }
 }
